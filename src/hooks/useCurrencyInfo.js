163d35bfede1aaf5ccbea9c52e1e76a3c2cfe6c5
Salman Zulfiqar Shaikh <salmanzulfiqar04@gmail.com>
2025-09-01 02:49:34 +0500

    added api link through fetch in the custom hook useCurrencyInfo

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency){
-     
-}
+     useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/{apiVersion}/{endpoint}`)
+     }, [currency])};
