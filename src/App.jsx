c203bdc9f08023a70370eca8e262d7165b799b1c
Salman Zulfiqar Shaikh <salmanzulfiqar04@gmail.com>
2025-09-04 04:34:56 +0500

    custom hook designed!!!

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import useCurrencyInfo from "./hooks/useCurrencyInfo";
 import "./App.css";
 
 function App() {
- 
+ const currencyData = useCurrencyInfo("usd");
+console.log("Currency Data in Component:", currencyData);
+return(
+  <h1>Hi</h1>
+)
 }
 
 export default App;
