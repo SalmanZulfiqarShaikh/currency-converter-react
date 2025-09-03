10909594c5026d0bb1d6174e0b0c4ea025945120
Salman Zulfiqar Shaikh <salmanzulfiqar04@gmail.com>
2025-09-04 04:55:54 +0500

    added inptBox from HC's Repo

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import useCurrencyInfo from "./hooks/useCurrencyInfo";
 import "./App.css";
 
 function App() {
- const currencyData = useCurrencyInfo("usd");
-console.log("Currency Data in Component:", currencyData);
+ 
 return(
-  <h1>Hi</h1>
+  <>
+    
+  </>
 )
 }
 
